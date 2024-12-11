359f08ab946952921ebe60a4adf4d069cb1dfab5
Ifra Muhammad Hashim <ifra@dcaptech.org>
2024-12-11 16:11:58 +0500

    project

---
+++ data.js
@@ -0,0 +1,63 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Non-StickFryPan-Black",
+    "price": 1200.00,
+    "description": "Best Non-Stick Round Frying Pan (3MM) - 18 cm available in 3 colors with.",
+    "image": "product/frypan1.webp"
+}, {
+    "id": 2,
+    "title": "Marble Coating Frypan",
+    "price": 1949.00,
+    "description": "Best Non-Stick Marble Coating Round Frying Pan (4MM) - 18 cm.",
+    "image": "product/frypan2.webp"
+}, {
+    "id": 3,
+    "title": "Mini FryPan",
+    "price": 849.00,
+    "description": "Best Quality Non Stick One Egg Frying Pan Mini Fry Pan 12 cm.",
+    "image": "product/FRYPAN3.avif"
+}, {
+    "id": 4,
+    "title": "melamine Dinner Set",
+    "price": 8599,
+    "description": "Majestic Melamine Dinner Set Crystal Coated Dinnerware 6Persons Serving-28 Pcs Square Flower.",
+    "image": "product/dinnerset1.webp"
+}, {
+    "id": 5,
+    "title": "crystal Dinnerset",
+    "price": 19999,
+    "description": "Majestic Luxury Crystal Coated Melamine Dinner Ware 72 Pcs DinningSet-8Persons Serving.",
+    "image": "product/dinnerSet2.webp"
+}, {
+    "id": 6,
+    "title": "Luxury DinnerSet",
+    "price": 19499,
+    "description": "Majestic Luxury Crystal Coated Melamine Dinner Ware 72 Pcs Dinning Set-8Persons Serving.",
+    "image": "product/dinnerset3.webp"
+}, {
+    "id": 7,
+    "title": "Trukish Hot-Pot",
+    "price": 2799,
+    "description": "Best Quality Turkish Style Naan And Roti Serving Box / Hot Pot With Lid.",
+    "image": "product/hotpot1.webp"
+}, {
+    "id": 8,
+    "title": "Steel Hot-Pot",
+    "price": 3499,
+    "description": "Majestic Chef Stainless Steel Clarion Hot Pot With Glass Lid - Brown - Large 4 L.",
+    "image": "product/hotpot2.webp"
+}, {
+    "id": 9,
+    "title": "Hot-pot Set 3pcs",
+    "price": 3999,
+    "description": "EPIC Series 3pcs Premium Quality Thermo Casserole Insulated / Hotpot Set - Insulated Food Warmer.",
+    "image": "product/hotpot3.webp"
+},
+]
